b9f13a1bbb79a24272aaabeef1e7e9163fa49402
ckoled <christian@mksrc.com>
2022-07-07 14:24:27 -0700

    basic be implement for canvas

---
+++ bff/src/db/ckCanvasTable.ts
@@ -0,0 +1,6 @@
+import Table from './Table';
+
+class CkCanvasTbl extends Table {
+  
+}
+export default new CkCanvasTbl('ck_canvas', { hasCreated: false, hasLastModified: false});
--- bff/src/db/index.ts
+++ bff/src/db/index.ts
@@ -1,5 +1,6 @@
 import * as db from './db';
 import ckSessionTbl from './ckSessionTable';
 import ckUserTbl from './ckUserTable';
+import ckCanvasTbl from './ckCanvasTable';
 
-export { db, ckSessionTbl, ckUserTbl };
+export { db, ckSessionTbl, ckUserTbl, ckCanvasTbl };
