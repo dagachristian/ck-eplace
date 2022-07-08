f873c61fc468a2eb915fa370b1836751e5167fb2
ckoled <christian@mksrc.com>
2022-07-08 15:05:18 -0700

    updated test

--- common/src/functions/.index.spec.ts
+++ common/src/functions/.index.spec.ts
@@ -4,43 +4,40 @@ import path from 'path';
 import { b8ToBMP, b8ToPNG, updBMPb8, updPNGb8 } from './canvasFunctions';
 
 const out = 'test_output/';
+beforeAll(() => fs.mkdirSync(path.resolve(__dirname, out), { recursive: true }))
 
 describe('functions', () => {
-  fs.mkdirSync(path.resolve(__dirname, out), { recursive: true });
   describe('canvas', () => {
+    const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    const buffer = Buffer.from(vals);
+    let rgb: number[][] = []
+    vals.forEach(v => {
+      rgb.push([(v >> 5) * 32, ((v & 28) >> 2) * 32, (v & 3) * 64])
+    })
+    console.log(rgb)
+    
+    let png: Buffer;
     it('b8topng', () => {
-      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-      const ret = b8ToPNG(Buffer.from(vals));
-      let rgb: number[][] = []
-      vals.forEach(v => {
-        rgb.push([(v >> 5) * 32, ((v & 28) >> 2) * 32, (v & 3) * 64])
-      })
-      console.log(out)
-      fs.writeFileSync(path.resolve(__dirname, out, 'out1.png'), ret);
-      expect(ret).toBeInstanceOf(Buffer);
+      png = b8ToPNG(buffer);
+      fs.writeFileSync(path.resolve(__dirname, out, 'out1.png'), png);
+      expect(png[1]).toBe('P'.charCodeAt(0));
     })
-
     it('updpngb8', () => {
-      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-      const o = b8ToPNG(Buffer.from(vals));
-      const n = updPNGb8(o, 0, 0, 200);
-      fs.writeFileSync(path.resolve(__dirname, out, 'out2.png'), n);
-      expect(n).toBeInstanceOf(Buffer);
+      updPNGb8(png, 0, 0, 200);
+      fs.writeFileSync(path.resolve(__dirname, out, 'out2.png'), png);
+      expect(png[1]).toBe('P'.charCodeAt(0));
     })
-
+    
+    let bmp: Buffer;
     it('b8tobmp', () => {
-      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-      const ret = b8ToBMP(Buffer.from(vals));
-      fs.writeFileSync(path.resolve(__dirname, out, 'out1.bmp'), ret);
-      expect(ret).toBeInstanceOf(Buffer);
+      bmp = b8ToBMP(buffer);
+      fs.writeFileSync(path.resolve(__dirname, out, 'out1.bmp'), bmp);
+      expect(bmp[0]).toBe('B'.charCodeAt(0));
     })
-
     it('updbmpb8', () => {
-      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-      const o = b8ToBMP(Buffer.from(vals));
-      updBMPb8(o, 0, 0, 200);
-      fs.writeFileSync(path.resolve(__dirname, out, 'out2.bmp'), o);
-      expect(o).toBeInstanceOf(Buffer);
+      updBMPb8(bmp, 0, 0, 200);
+      fs.writeFileSync(path.resolve(__dirname, out, 'out2.bmp'), bmp);
+      expect(bmp[0]).toBe('B'.charCodeAt(0));
     })
   })
 })
