ca466abf3bbb4202730406f0f9f60423c91f2024
ckoled <christian@mksrc.com>
2022-07-08 13:17:16 -0700

    minor update and temp pixel mani

--- bff/src/services/canvasService.ts
+++ bff/src/services/canvasService.ts
@@ -1,7 +1,7 @@
 import { client } from '../repositories/redis';
 
-export const fullCanvas = async (): Promise<number[]> => {
+export const fullCanvas = async (): Promise<Buffer> => {
   let canvas: string[] | number[] = await client.lRange('canvas', 0, -1);
   canvas = canvas.map(v => parseInt(v));
-  return canvas;
+  return Buffer.from(canvas);
 }
--- common/src/functions/.index.spec.ts
+++ common/src/functions/.index.spec.ts
@@ -1,17 +1,21 @@
 import fs from 'fs';
+import path from 'path';
 import { PNG } from 'pngjs';
 
-import { bytesToPNG } from './canvasFunctions';
+import { b8ToPNG } from './canvasFunctions';
 
 describe('functions', () => {
   it('bytetopng', () => {
     const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-    const ret = bytesToPNG(vals);
+    const ret = b8ToPNG(Buffer.from(vals));
     let out: number[][] = []
     vals.forEach(v => {
       out.push([(v >> 5) * 32, ((v & 28) >> 2) * 32, (v & 3) * 64])
     })
     console.log(out)
+    const ri = PNG.sync.read(ret)
+    const wi = PNG.sync.write(ri)
+    fs.writeFileSync(path.resolve(__dirname, 'out.png'), wi);
     expect(ret).toBeInstanceOf(Buffer);
   })
 })
