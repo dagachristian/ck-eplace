14b9be0e13f7ad67a822a7de2115d9f45e1b5808
ckoled <christian@mksrc.com>
2022-07-08 14:25:15 -0700

    bmp and update bmp, using that

--- common/src/functions/.index.spec.ts
+++ common/src/functions/.index.spec.ts
@@ -2,20 +2,43 @@ import fs from 'fs';
 import path from 'path';
 import { PNG } from 'pngjs';
 
-import { b8ToPNG } from './canvasFunctions';
+import { b8ToBMP, b8ToPNG, updBMPb8, updPNGb8 } from './canvasFunctions';
 
 describe('functions', () => {
-  it('bytetopng', () => {
-    const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-    const ret = b8ToPNG(Buffer.from(vals));
-    let out: number[][] = []
-    vals.forEach(v => {
-      out.push([(v >> 5) * 32, ((v & 28) >> 2) * 32, (v & 3) * 64])
+  describe('canvas', () => {
+    it('b8topng', () => {
+      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+      const ret = b8ToPNG(Buffer.from(vals));
+      let out: number[][] = []
+      vals.forEach(v => {
+        out.push([(v >> 5) * 32, ((v & 28) >> 2) * 32, (v & 3) * 64])
+      })
+      console.log(out)
+      fs.writeFileSync(path.resolve(__dirname, 'out1.png'), ret);
+      expect(ret).toBeInstanceOf(Buffer);
     })
-    console.log(out)
-    const ri = PNG.sync.read(ret)
-    const wi = PNG.sync.write(ri)
-    fs.writeFileSync(path.resolve(__dirname, 'out.png'), wi);
-    expect(ret).toBeInstanceOf(Buffer);
+
+    it('updpngb8', () => {
+      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+      const o = b8ToPNG(Buffer.from(vals));
+      const n = updPNGb8(o, 0, 0, 200);
+      fs.writeFileSync(path.resolve(__dirname, 'out2.png'), n);
+      expect(n).toBeInstanceOf(Buffer);
+    })
+
+    it('b8tobmp', () => {
+      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+      const ret = b8ToBMP(Buffer.from(vals));
+      fs.writeFileSync(path.resolve(__dirname, 'out1.bmp'), ret);
+      expect(ret).toBeInstanceOf(Buffer);
+    })
+
+    it('updbmpb8', () => {
+      const vals = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+      const o = b8ToBMP(Buffer.from(vals));
+      updBMPb8(o, 0, 0, 200);
+      fs.writeFileSync(path.resolve(__dirname, 'out2.bmp'), o);
+      expect(o).toBeInstanceOf(Buffer);
+    })
   })
 })
