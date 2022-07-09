63e2a58c8a3674278b4cf374e285a3fa8a0c7c86
ckoled <christian@mksrc.com>
2022-07-08 18:14:21 -0700

    switched to ioredis

--- rdb/redis.js
+++ rdb/redis.js
@@ -1,14 +1,12 @@
-var redis = require('redis');
-var client = redis.createClient({
-  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
-});
-client.connect().then(() => {
-  client.del('canvas').then(() => {
-    var size = parseInt(process.env.CANVAS_SIZE);
-    var vals = [];
-    for (var i=0;i<size*size;i++) {
-      vals.push(Math.floor(Math.random()*255).toString())
-    }
-    client.lPush('canvas', vals).then(() => client.disconnect())
-  })
-});
+var Redis = require('ioredis');
+var client = new Redis(
+  `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+)
+client.del('canvas').then(() => {
+  var size = parseInt(process.env.CANVAS_SIZE);
+  var vals = [];
+  for (var i=0;i<size*size;i++) {
+    vals.push(Buffer.from([Math.floor(Math.random()*255)]))
+  }
+  client.lpush('canvas', vals).then(() => client.disconnect())
+})
